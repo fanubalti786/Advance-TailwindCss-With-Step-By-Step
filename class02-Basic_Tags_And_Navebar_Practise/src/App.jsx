58b70e923d7663354431a3d0dc53e1be711781ca
fanubalti786 <fanubalti786@gmail.com>
2025-01-27 19:26:12 +0500

    update

---
+++ class02-Basic_Tags_And_Navebar_Practise/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul className="flex bg-linear-to-r/decreasing from-indigo-500 to-teal-400 text-white py-2 text-lg">
+          <li className="mx-4">Home</li>
+          <li className="mx-4">About</li>
+          <li className="mx-4">Contact us</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
