2deb48ec44fa131df523692ffc353bb088a8a0ff
fanubalti786 <fanubalti786@gmail.com>
2025-01-27 22:22:29 +0500

    update

--- class01-Installation_Of_TailwindCss_With_React_Vite/src/App.jsx
+++ class01-Installation_Of_TailwindCss_With_React_Vite/src/App.jsx
@@ -3,7 +3,15 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline bg-amber-100">Hello world!</h1>
+      <div>
+      <nav>
+        <ul className="flex bg-linear-to-r/decreasing from-indigo-500 to-teal-400 text-white py-2 text-lg">
+          <li className="mx-4">Home</li>
+          <li className="mx-4">About</li>
+          <li className="mx-4">Contact us</li>
+        </ul>
+      </nav>
+    </div>
     </>
   );
 }
